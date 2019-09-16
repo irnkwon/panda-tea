8f46065b86a092a35ab820a47916b123149e65f3
JadenAhn <emailtojaden@gmail.com>
2019-09-16 12:38:53 -0400

    - Updated ASP.CORE version to 2.2 - Added MVC web page based application - Added Model class Scaffolding SqlServer - Added controllers - Added temporary link to each controllers to the home page - Added web hosting for IIS

---
+++ PandaTea/Models/StoreTbl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PandaTea.Models
+{
+    public partial class StoreTbl
+    {
+        public StoreTbl()
+        {
+            TransactionTbl = new HashSet<TransactionTbl>();
+        }
+
+        public decimal StoreId { get; set; }
+        public string StoreName { get; set; }
+        public string Address { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
+        public virtual ICollection<TransactionTbl> TransactionTbl { get; set; }
+    }
+}
