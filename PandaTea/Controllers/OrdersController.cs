323bbbc41d5b537fa9b89447d881470b8f5da4e9
JadenAhn <emailtojaden@gmail.com>
2019-10-27 16:44:26 -0400

    Added Orders controller

---
+++ PandaTea/Controllers/OrdersController.cs
@@ -0,0 +1,246 @@
+﻿/* OrderController.cs
+ * 
+ * PROG3050: Programming Microsoft Enterprise Applications
+ * Group 7
+ * 
+ * Revision History
+ *          Ji Hong Ahn, 2019-09-12: Created
+ *          Ji Hong Ahn, 2019-10-10: Refined codes
+ *                                   Added documentation comments and header comments
+ *          Samantha Dang, 2019-10-23: Add user identification
+ *          Samantha Dang, 2019-10-25: Sessions for items added to order (StoreQuantity, Create)
+ *  */
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PandaTea.Models;
+
+namespace PandaTea.Controllers
+{
+    /// <summary>
+    /// Controller class for OrderModel
+    /// </summary>
+    public class OrdersController : Controller
+    {
+        private readonly PandaTeaContext _context;
+
+        public OrdersController(PandaTeaContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Returns View for Index action
+        /// View with Order data if the user is logged in. A user only sees their own orders. If the user is not logged in, they are prompted to log in.
+        /// </summary>
+        /// <returns>A View with list of User's orders or redirects user to log in screen</returns>
+        public async Task<IActionResult> Index()
+        {
+            if (HttpContext.Session.GetString("UserId") != null)
+            {
+                decimal userId = Decimal.Parse(HttpContext.Session.GetString("UserId"));
+                return View(await _context.Order.Where(o => o.UserId == userId).ToListAsync());
+            }
+            else
+            {
+                TempData["LoginRequiredMessage"] = "Please log in to view your orders.";
+                return RedirectToAction("Login", "User");
+            }
+        }
+
+        /// <summary>
+        /// Store the Quantity of drinks the user wants
+        /// </summary>
+        /// <returns>Status and message</returns>
+        public ActionResult StoreQuantity(Order order)
+        {
+            try
+            {
+                var qty = order.Quantity;
+                HttpContext.Session.SetString("Quantity", qty.ToString());
+                return Json(new { status = true, message = "Quantity storage successful: " + qty });
+            }
+            catch (Exception e)
+            {
+                return Json(new { status = false, message = "Quantity storage failed: " + e });
+            }
+        }
+
+        /// <summary>
+        /// Returns View for Details action
+        /// </summary>
+        /// <param name="id">OrderId to select</param>
+        /// <returns>View with Order data</returns>
+        public async Task<IActionResult> Details(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .Include(o => o.Menu)
+                .Include(o => o.Store)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        /// <summary>
+        /// Returns View for Create action
+        /// </summary>
+        /// <returns>View</returns>
+        public IActionResult Create()
+        {
+            ViewData["MenuId"] = new SelectList(_context.Menu, "MenuId", "MenuId");
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "StoreId");
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId");
+            return View();
+        }
+
+
+        /// <summary>
+        /// Inserts Order data to the database
+        /// </summary>
+        /// <param name="order">Order data to insert</param>
+        /// <returns>View with Order data</returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("OrderId,UserId,MenuId,StoreId,Quantity,DatePurchased")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menu, "MenuId", "MenuId", order.MenuId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "StoreId", order.StoreId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", order.UserId);
+            return View(order);
+        }
+
+        /// <summary>
+        /// Returns View for Edit action
+        /// </summary>
+        /// <param name="id">OrderId to update</param>
+        /// <returns>View with Order data</returns>
+        public async Task<IActionResult> Edit(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menu, "MenuId", "MenuId", order.MenuId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "StoreId", order.StoreId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", order.UserId);
+            return View(order);
+        }
+
+        /// <summary>
+        /// Updates Order data with a OrderId to the database
+        /// </summary>
+        /// <param name="id">OrderId to update</param>
+        /// <param name="orderModel">Order data to update</param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(decimal id, [Bind("OrderId,UserId,MenuId,StoreId,Quantity,DatePurchased")] Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.OrderId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuId"] = new SelectList(_context.Menu, "MenuId", "MenuId", order.MenuId);
+            ViewData["StoreId"] = new SelectList(_context.Store, "StoreId", "StoreId", order.StoreId);
+            ViewData["UserId"] = new SelectList(_context.User, "UserId", "UserId", order.UserId);
+            return View(order);
+        }
+
+        /// <summary>
+        /// Returns View for Delete action
+        /// </summary>
+        /// <param name="id">OrderId to delete</param>
+        /// <returns>View with Order data</returns>
+        public async Task<IActionResult> Delete(decimal? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Order
+                .Include(o => o.Menu)
+                .Include(o => o.Store)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        /// <summary>
+        /// Deletes Order data with a given OrderId from the database
+        /// </summary>
+        /// <param name="id">OrderId to delete</param>
+        /// <returns>IActionResult to return to Index action</returns>
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(decimal id)
+        {
+            var order = await _context.Order.FindAsync(id);
+            _context.Order.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        /// <summary>
+        /// Verifies if Order data with the given OrderId exists
+        /// </summary>
+        /// <param name="id">OrderId to verify</param>
+        /// <returns>true if a Order exists</returns>
+        private bool OrderExists(decimal id)
+        {
+            return _context.Order.Any(e => e.OrderId == id);
+        }
+    }
+}
